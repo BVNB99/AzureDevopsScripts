7d2870d38c34857719904d61ffbe6bc5372f8e95
BVNB99 <venkatanagababu.battina@cognizant.com>
2024-08-03 07:59:53 +0530

    Create main.tf

---
+++ Terraform/storage_account/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "create_rg" {
+    name       =       "test-rg"
+    location   =       "East US"
+}
+
+resource "azurerm_storage_account" "create_storage" {
+    name        =       "tfbackendstorage1"
+    resource_group_name         =       "test-rg" #azurerm_resource_group.create_rg.name
+    location                    =       "East US" #azurerm_resource_group.create_rg.location
+    account_tier                =       "Standard"
+    account_replication_type    =       "LRS"
+    tags = {
+        environment     =       "terraform-backend"
+    }
+}
+
+resource "azurerm_storage_container" "create_container" {
+    name                        =       "terraform-state-test"
+    storage_account_name        =       azurerm_storage_account.create_storage.name
+    container_access_type       =       "private"
+}
+
+
