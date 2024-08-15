764349608ceb7a17f16ea44450aff820b5056cb8
BVNB99 <venkatanagababu.battina@cognizant.com>
2024-08-15 10:54:27 +0530

    Update main.tf

--- Terraform/create_container_registry/main.tf
+++ Terraform/create_container_registry/main.tf
@@ -2,15 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "create_rg" {
-    name        =       "test-rg"
-    location    =       "East US"
-}
+#resource "azurerm_resource_group" "create_rg" {
+#    name        =       "test-rg"
+#    location    =       "East US"
+#}
 
 resource "azurerm_container_registry" "create_container" {
     name        =       "democicdar"
-    resource_group_name =       azurerm_resource_group.create_rg.name
-    location            =       azurerm_resource_group.create_rg.location
+    resource_group_name =       "cluster-rg"   #azurerm_resource_group.create_rg.name
+    location            =       "East US"      #azurerm_resource_group.create_rg.location
     sku                 =       "Basic"
     admin_enabled       =       false
 }
